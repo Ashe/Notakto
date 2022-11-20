84caa06b761d945cf1154b74a73bd2481cd07028
ashe <git@aas.sh>
2022-11-20 16:03:05 +0000

    LookAt targets now tracked and rendered

--- src/lib/Rendering.hs
+++ src/lib/Rendering.hs
@@ -2,6 +2,7 @@ module Rendering (
   render
 ) where
 
+import Control.Monad (when)
 import Foreign.C.Types (CFloat(..))
 
 import Apecs
@@ -36,19 +37,22 @@ render = do
 
 renderAimRay :: System World ()
 renderAimRay = do
-  Aim ray <- get global
+  Aim ray _ <- get global
   let lineStart = addVectors (RL.ray'position ray) (Vector3 0 (-0.05) 0)
       lineEnd = addVectors (RL.ray'position ray) $ multiplyVector (RL.ray'direction ray) 10
   liftIO $ RL.drawLine3D lineStart lineEnd RL.yellow
 
 
 renderBoards :: System World ()
-renderBoards = cmapM_ renderBoard
+renderBoards = do
+  Aim _ target <- get global
+  cmapM_ (renderBoard target)
 
 
-renderBoard :: (BoardComponent, PositionComponent) -> System World ()
-renderBoard (b, Position p) = do
-  renderCrosses p b
+renderBoard :: LookAtTarget -> (BoardComponent, PositionComponent, Entity) ->
+               System World ()
+renderBoard target (b, Position p, e) = do
+  renderCrosses p (b, e) target
   liftIO $ do
     RL.drawCube (addVectors p $ Vector3 0.5    0 0) t 3 t RL.white
     RL.drawCube (addVectors p $ Vector3 (-0.5) 0 0) t 3 t RL.white
@@ -57,23 +61,34 @@ renderBoard (b, Position p) = do
   where t = 0.05
 
 
-renderCrosses :: Vector3 -> BoardComponent -> System World ()
-renderCrosses origin b = do
-  renderCross origin (-1)   1  (_tl b)
-  renderCross origin   0    1  (_tc b)
-  renderCross origin   1    1  (_tr b)
-  renderCross origin (-1)   0  (_ml b)
-  renderCross origin   0    0  (_mc b)
-  renderCross origin   1    0  (_mr b)
-  renderCross origin (-1) (-1) (_bl b)
-  renderCross origin   0  (-1) (_bc b)
-  renderCross origin   1  (-1) (_br b)
+renderCrosses :: Vector3 -> (BoardComponent, Entity) -> LookAtTarget ->
+                 System World ()
+renderCrosses origin (b, e) target = do
+  renderCross origin (-1)   1  (_tl b) (isAimingAtCell e 0 target)
+  renderCross origin   0    1  (_tc b) (isAimingAtCell e 1 target)
+  renderCross origin   1    1  (_tr b) (isAimingAtCell e 2 target)
+  renderCross origin (-1)   0  (_ml b) (isAimingAtCell e 3 target)
+  renderCross origin   0    0  (_mc b) (isAimingAtCell e 4 target)
+  renderCross origin   1    0  (_mr b) (isAimingAtCell e 5 target)
+  renderCross origin (-1) (-1) (_bl b) (isAimingAtCell e 6 target)
+  renderCross origin   0  (-1) (_bc b) (isAimingAtCell e 7 target)
+  renderCross origin   1  (-1) (_br b) (isAimingAtCell e 8 target)
 
 
-renderCross :: Vector3 -> Float -> Float -> Cell -> System World ()
-renderCross _ _ _ Empty = pure ()
-renderCross origin i j Filled = liftIO $ do
+isAimingAtCell :: Entity -> Int -> LookAtTarget -> Bool
+isAimingAtCell (Entity e) i (Target (Entity e') i') = e == e' && i == i'
+isAimingAtCell _ _ _ = False
+
+
+renderCross :: Vector3 -> Float -> Float -> Cell -> Bool -> System World ()
+renderCross _ _ _ Empty False = pure ()
+
+renderCross origin i j Filled _ = liftIO $ do
   RL.drawLine3D (f (-0.4) (-0.4)) (f 0.4 0.4) RL.red
   RL.drawLine3D (f 0.4 (-0.4)) (f (-0.4) 0.4) RL.red
   where center = addVectors origin $ Vector3 (CFloat i) (CFloat j) 0
         f x y = addVectors center $ Vector3 x y 0
+
+renderCross origin i j Empty True = liftIO $ do
+    RL.drawCircle3D center 0.4 (Vector3 0 1 0) 0 RL.yellow
+  where center = addVectors origin $ Vector3 (CFloat i) (CFloat j) 0
--- src/lib/Util.hs
+++ src/lib/Util.hs
@@ -1,7 +1,9 @@
 module Util (
   addVectors,
+  subtractVectors,
   multiplyVector,
   multiplyVectors,
+  magnitudeVector
 ) where
 
 import Foreign.C.Types (CFloat(..))
@@ -17,6 +19,13 @@ addVectors a b = Vector3
   (vector3'z a + vector3'z b)
 
 
+subtractVectors :: Vector3 -> Vector3 -> Vector3
+subtractVectors a b = Vector3
+  (vector3'x a - vector3'x b)
+  (vector3'y a - vector3'y b)
+  (vector3'z a - vector3'z b)
+
+
 multiplyVector :: Vector3 -> Float -> Vector3
 multiplyVector a b = let b' = CFloat b in Vector3
   (vector3'x a * b')
@@ -29,3 +38,8 @@ multiplyVectors a b = Vector3
   (vector3'x a * vector3'x b)
   (vector3'y a * vector3'y b)
   (vector3'z a * vector3'z b)
+
+
+magnitudeVector :: Vector3 -> Float
+magnitudeVector (Vector3 x y z) =
+  let CFloat f = sqrt $ (x * x) + (y * y) + (z * z) in f
